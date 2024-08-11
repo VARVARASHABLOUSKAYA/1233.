3a260805af454b4f047a852eabccf94ad6ff4d40
VARVARASHABLOUSKAYA VARVARASHABLOUSKAYA <varavarashablouskaya@gmail.com>
2024-08-11 08:43:50 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("rvfe","2qf")
+        ERC721("3efewf","vec")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
